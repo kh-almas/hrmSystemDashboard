37f81ba22d1dfb610de8b0a2f921f0f39ef0f1f3
iambijoy656 <bijoychandrodas56@gmail.com>
2023-08-02 19:30:03 +0600

    add pagination

--- src/components/dashboard/inventoryManagement/sales/SaleReturn.js
+++ src/components/dashboard/inventoryManagement/sales/SaleReturn.js
@@ -73,34 +73,31 @@ const SaleReturn = () => {
               </div>
             </div>
             <div className="d-flex justify-content-between align-items-center">
-              <p style={{ fontSize: "13px" }}>Showing page 1 of 1</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <button
-                  class="btn btn-pill btn-outline-warning btn-xs"
-                  type="button"
-                >
-                  <i
-                    style={{ fontSize: "24px" }}
-                    className="icofont icofont-swoosh-left"
-                  ></i>
-                </button>
+              <button
+                class="btn btn-pill btn-outline-secondary btn-xs"
+                type="button"
+              >
+                <i
+                  style={{ fontSize: "24px" }}
+                  className="icofont icofont-swoosh-left"
+                ></i>
+              </button>
 
-                <p
-                  style={{ fontSize: "13px" }}
-                  className="p-1 px-2 btn-warning-gradien mt-3 mx-2 rounded text-light"
-                >
-                  1
-                </p>
-                <button
-                  class="btn btn-pill btn-outline-warning btn-xs"
-                  type="button"
-                >
-                  <i
-                    style={{ fontSize: "24px" }}
-                    className="icofont icofont-swoosh-right"
-                  ></i>
-                </button>
-              </div>
+              <p
+                style={{ fontSize: "13px" }}
+                className="p-1 px-2 btn-primary-gradien mt-3 mx-2 rounded text-light"
+              >
+                1
+              </p>
+              <button
+                class="btn btn-pill btn-outline-secondary btn-xs"
+                type="button"
+              >
+                <i
+                  style={{ fontSize: "24px" }}
+                  className="icofont icofont-swoosh-right"
+                ></i>
+              </button>
             </div>
           </div>
         </div>
