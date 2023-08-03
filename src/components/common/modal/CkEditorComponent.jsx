269c845b2d57dd73edc7945926c9983c76ad2ba0
kh-almas <services.almas@gmail.com>
2023-08-03 17:44:21 +0600

    small update

--- src/components/common/modal/CkEditorComponent.jsx
+++ src/components/common/modal/CkEditorComponent.jsx
@@ -3,29 +3,29 @@ import CKEditors from "react-ckeditor-component";
 
 const CkEditorComponent = ({label, fieldContent}) => {
     const [content,setContent] = useState('content')
-    const onChange = (evt) => {
+    const onSubmit = (evt) => {
         const newContent = evt.editor.getData();
         setContent(newContent)
     }
     return (
-        <Fragment>
+        <>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-sm-12">
-                        <label className="text-muted" htmlFor="exampleFormControlSelect9">{label}</label>
+                        <label style={{ color: "#8990b6", fontSize: "16px" }} htmlFor="exampleFormControlSelect9">{label}</label>
                         <div className="card">
                             <CKEditors
                                 activeclassName="p10"
                                 content={fieldContent}
                                 events={{
-                                    "change": onChange
+                                    "change": onSubmit
                                 }}
                             />
                         </div>
                     </div>
                 </div>
             </div>
-        </Fragment>
+        </>
     );
 }
 export default CkEditorComponent;
